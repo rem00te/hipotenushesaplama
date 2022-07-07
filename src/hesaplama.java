047a2301b5443ffe55f3b13f4504fb19061b53be
Onur ALTINTOP <rem00te@hotmail.com>
2022-07-07 09:32:04 +0300

    Hipotenüs Hesaplama Uygulaması

---
+++ src/hesaplama.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class hesaplama {
+    public static void main(String[] args) {
+        int a, b;
+        double c;
+        Scanner input = new Scanner(System.in);
+        System.out.print("1.kenar  :");
+         a = input.nextInt();
+
+
+        System.out.print("2.kenar  :");
+        b = input.nextInt();
+
+
+        c = Math.sqrt((a*a) + (b*b));
+
+        System.out.println("Hipotenus :" +c);
+
+    }
+}
